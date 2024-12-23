2a466763a7dfd3e32b7d834723f92391e2e4a36d
simenesky <simenesky@gmail.com>
2024-12-23 21:01:31 +0100

    small change

--- src/app/modules/pages/contact-location/contact-location.component.ts
+++ src/app/modules/pages/contact-location/contact-location.component.ts
@@ -10,7 +10,7 @@ export class ContactLocationComponent {
   options: google.maps.MapOptions = {
     mapId: "DEMO_MAP_ID",
     center: { lat: 42.4621742, lng: 18.5132191 },
-    zoom: 17,
+    zoom: 17, 
   };
 
 
