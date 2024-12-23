81ae55d6a227f95317530521de9f8d794a430296
simenesky <simenesky@gmail.com>
2024-12-23 18:21:55 +0100

    added nativ menu items

--- src/app/modules/components/navbar/navbar.component.ts
+++ src/app/modules/components/navbar/navbar.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isMenuOpen:boolean = false;
+
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(){
+    this.isMenuOpen = false;
+  }
+
 }
