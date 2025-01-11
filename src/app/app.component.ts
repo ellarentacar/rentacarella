311570a3c16c1cc594f49b8e61b4fefc6daf75f2
simenesky <simenesky@gmail.com>
2025-01-11 11:06:44 +0100

    added meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'rentacarella';
+export class AppComponent implements OnInit{
+  title = 'rent a car ella herceg novi';
+  constructor(private metaService: Meta) {}
+  
+  ngOnInit() {
+    this.metaService.addTags([
+      { name: 'description', content: 'rent a car herceg novi' },
+      { name: 'keywords', content: 'rent, car, herceg novi, igalo, montenegro,crna gora, dubrovnik ' },
+      { name: 'author', content: 'rent a car ella' },
+    ]);
+  }
+
 }
